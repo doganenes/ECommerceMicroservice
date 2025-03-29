736ca1ba1d0d65ef0df96966c16d103e1d60b8f7
Enes Doğan <enesdoganlive@hotmail.com>
2025-03-29 17:48:31 +0300

    Create second microservice

---
+++ Services/Discount/Multishop.Discount/Context/DapperContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Multishop.Discount.Entities;
+using System.Data;
+
+namespace Multishop.Discount.Context
+{
+    public class DapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public DapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
