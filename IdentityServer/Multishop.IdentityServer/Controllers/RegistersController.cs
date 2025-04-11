6282f50d3e33f5d90102dc79400d156947047efa
Enes Doğan <enesdoganlive@hotmail.com>
2025-04-11 16:15:31 +0300

    identityserver migration

---
+++ IdentityServer/Multishop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Multishop.IdentityServer.Dtos;
+using Multishop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace Multishop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto dto)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName = dto.Username,
+                Email = dto.Email,
+                Name = dto.Name,
+                Surname = dto.Surname,
+            };
+
+            var result = await _userManager.CreateAsync(values, dto.Password);
+            if (result.Succeeded) {
+                return Ok("User registered successfully!");
+            }
+            else
+            {
+                return Ok("An error occured while user registration!");
+            }
+        }
+    }
+}
