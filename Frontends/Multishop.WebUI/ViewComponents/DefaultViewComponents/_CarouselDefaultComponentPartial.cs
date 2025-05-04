7fc10dfa10812482978fbf07816cb38c33b281d8
Enes Doğan <enesdoganlive@hotmail.com>
2025-05-04 21:57:18 +0300

    layout

---
+++ Frontends/Multishop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _CarouselDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/Multishop.WebUI/ViewComponents/UILayoutViewComponents/_TopbarUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _TopbarUILayoutComponentPartial : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+
+}
