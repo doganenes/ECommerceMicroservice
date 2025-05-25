a800b892bc941e9c2a07755d48b3625004816bc9
Enes Doğan <enesdoganlive@hotmail.com>
2025-05-25 23:53:47 +0300

    update

--- Frontends/Multishop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/Multishop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -26,7 +26,7 @@ namespace Multishop.WebUI.Areas.Admin.Controllers
             ViewBag.v3 = "Category Operations";
 
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");
+            var responseMessage = await client.GetAsync("http://localhost:7070/api/Categories");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -55,7 +55,7 @@ namespace Multishop.WebUI.Areas.Admin.Controllers
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(dto);
             StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("https://localhost:7070/api/Categories", stringContent);
+            var responseMessage = await client.PostAsync("http://localhost:7070/api/Categories", stringContent);
 
             if (responseMessage.IsSuccessStatusCode)
             {
@@ -72,7 +72,7 @@ namespace Multishop.WebUI.Areas.Admin.Controllers
         public async Task<IActionResult> DeleteCategory(string id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.DeleteAsync($"https://localhost:7070/api/Categories?id=" + id);
+            var responseMessage = await client.DeleteAsync($"http://localhost:7070/api/Categories?id=" + id);
             if (responseMessage.IsSuccessStatusCode)
             { 
                 return RedirectToAction("Index", "Category", new { area = "Admin" });
@@ -89,7 +89,7 @@ namespace Multishop.WebUI.Areas.Admin.Controllers
             ViewBag.v2 = "Update Category";
             ViewBag.v3 = "Category Operations";
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7070/api/Categories/" + id);
+            var responseMessage = await client.GetAsync($"http://localhost:7070/api/Categories/" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -106,7 +106,7 @@ namespace Multishop.WebUI.Areas.Admin.Controllers
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(dto);
             StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
-            var responseMessage = await client.PutAsync($"https://localhost:7070/api/Categories/" , stringContent);
+            var responseMessage = await client.PutAsync($"http://localhost:7070/api/Categories/" , stringContent);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "Category", new { area = "Admin" });
