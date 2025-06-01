5f68ffda6b86eaba736c51e41f46d3e279b56b07
Enes Doğan <enesdoganlive@hotmail.com>
2025-06-01 20:17:16 +0300

    add user template

---
+++ Frontends/Multishop.WebUI/Areas/User/Controllers/CargoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Multishop.WebUI.Areas.User.Controllers
+{
+    [Area("User")]
+    public class CargoController : Controller
+    {
+        public IActionResult MyOrderList()
+        {
+            return View();
+        }
+    }
+}
