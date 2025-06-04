5686558aedabf5b348592e9e7ddc5286d3d69b38
Enes Doğan <enesdoganlive@hotmail.com>
2025-06-04 11:24:43 +0300

    update

--- Frontends/Multishop.WebUI/Controllers/ProductListController.cs
+++ Frontends/Multishop.WebUI/Controllers/ProductListController.cs
@@ -1,15 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Multishop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
 
 namespace Multishop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult ProductDetail()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+                return PartialView();
+        }
+
+                
+        
+        [HttpPost]
+        public IActionResult AddComment(CreateCommentDto dto)
         {
             return View();
         }
